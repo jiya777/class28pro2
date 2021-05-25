8a2678e2ce7968ba98a134bcd21b981d830ea511
jiya777 <72973968+jiya777@users.noreply.github.com>
2021-05-25 20:14:11 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,28 @@
+class Launcher{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+fly(){
+    this.launcher.bodyA=null;
+}
+
+attach(){
+    this.launcher.bodyA=Body;
+}
+       display(){
+           if(this.sling.bodyA){
+
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.sling.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }}
+    
+}
